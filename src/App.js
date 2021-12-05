266a9dc36adfa45e95e4971ece2b060cfc363d8d
Mahdi ben Zinouba <benzinoubamahdi@gmail.com>
2021-12-05 18:14:57 +0100

    changed from testing to test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>testing</h2>
+        <h2>test</h2>
         <p>
           Edit <code>src/jei.js</code>
         </p>
